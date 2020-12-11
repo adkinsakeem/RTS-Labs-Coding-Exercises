bbfd9800839b3ef332a8a856fb0312b87bb0adbf
Akeem Adkins <adkin@LAPTOP-QP5643IQ.pmusa.net>
2020-12-11 16:32:01 -0500

    Organized and labeled code for easy navigation

--- Above_And_Below/Above_And_Below.java
+++ Above_And_Below/Above_And_Below.java
@@ -5,11 +5,11 @@ public class Above_And_Below{
 		int belowNum = 0;
 
 		for(int arrSingleNum : arrNumbers){
-			if(arrSingleNum > initNumber)
-				aboveNum++;
+			if(arrSingleNum > initNumber)     /*Checks if the numbers in the Array is greater than the initNumber variable*/
+				aboveNum++;	    /*Adds to the aboveNum variable if it is*/
 			else if(arrSingleNum < initNumber)
-				belowNum++;
+				belowNum++;      /*Adds to the belowNum variable if it is not*/
 		}
-		System.out.println("above: " + aboveNum + ", below: " + belowNum);
+		System.out.println("above: " + aboveNum + ", below: " + belowNum);      /*Prints the results*/
 	}
 }
--- Above_And_Below/Values_For_Above_And_Below.java
+++ Above_And_Below/Values_For_Above_And_Below.java
@@ -4,11 +4,13 @@ public class Values_For_Above_And_Below{
 		
 		int[] newArray = getArray();
 		int initNumber = 9;
+		/*Sends the variables to the Array_2_InitValue_Check function in the Above_And_Below class*/
 		Above_And_Below.Array_2_InitValue_Check(initNumber, newArray);
 	}
 
-
 	private static int[] getArray(){
+
+		/*A list of arrays that can be used to test the Above_And_Below class*/
 		int[] newArray = {11, 15, 14, 4, 24, 8, 19, 10, 17, 13, 11, 16, 25, 3, 2, 25, 26, 7};
 		// int[] newArray = {30, 10, 2, 5, 23, 26};
 		// int[] newArray = {16, 11, 10, 27, 20, 7, 23, 11, 5, 4};
